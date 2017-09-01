34a6ed9b929b0f9fd74d4ddec3d1223457893be1
Wilfred Ruck <wilfredoruck@gmail.com>
2017-08-31 20:25:12 -0400

    update

---
+++ Maki-Zoo-CC/app/assets/javascripts/components/ErrorMessage.js.jsx
@@ -0,0 +1,14 @@
+class ErrorMessage extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    return (
+    	<div>
+    		<p>Invalid Input! (Can't be blank or repeated)</p>
+    	</div>
+    )
+  }
+}
+
---
+++ Maki-Zoo-CC/app/assets/javascripts/components/SuccessMessage.js.jsx
@@ -0,0 +1,14 @@
+class SuccessMessage extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    return (
+    	<div>
+    		<p>Animal Added Successfully!</p>
+    	</div>
+    )
+  }
+}
+
